9c373d8e9c79506f63ed51721138d7d3c92aee95
mfreitas-dev <mbkshfi@gmail.com>
2026-09-12 15:09:02 -0300

    arquivo criado e configurado para que o express e o cors funcionem, junto com uma rota /health para teste de funcionamento.

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'OK! API funcionando!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
